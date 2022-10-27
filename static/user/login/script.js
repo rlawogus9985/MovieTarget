7b56541f17ee51fa9fcd05ed7260ded6105b975b
KRK2S <i0kjkk@naver.com>
2022-10-27 09:10:10 +0900

    20221026
    회원가입 및 로그인시 무기입시 기입해달라는 script작성완료
    회원가입 및 로그인 template에서 script와 관련된 id camelCase로 재구성완료.

--- static/user/login/script.js
+++ static/user/login/script.js
@@ -4,58 +4,76 @@ https://codepen.io/ig_design/full/KKVQpVP
 * ========================================= */
 
 function loginFormCheck() {
-    
-    // 로그인 아이디 비밀번호 확인
-    let loginname = document.getElementById("loginname").value;
-    let loginpassword = document.getElementById("loginpassword").value;
-    
-    document.getElementById("username").value = loginname;
-    document.getElementById("password").value = loginpassword;
-    document.getElementById("loginForm").submit();
+  // Form에 제출하기전 user가 입력한 input태그의 id를 통하여 value를 변수명 객체에 초기화  
+  let loginname = document.getElementById("loginName").value;
+  let loginpassword = document.getElementById("loginPassword").value;
+  // let loginform = document.getElementById("loginForm").value;
+  // 로그인 아이디 및 패스워드 무입력 검사
+  if (loginname==0 || loginname=="") {
+    alert("ID를 입력해주세요.")
+    // loginform.loginname.focus();
+  }else if(loginpassword==0 || loginpassword=="") {
+    alert("PW를 입력해주세요.")
+    // loginform.loginpassword.focus();
   }
-  function SignupFormCheck() {
-    
-    // 아이디 검사
-    let idTxt = document.getElementById("signupname").value;
-    if (idTxt.length < 4 || idTxt.length > 12) {
-      alert("아이디 글자 수를 확인해 주세요.");
-      return false;
-      
-    }
-    // 비밀번호 글자수 검사
-    let pwTxt = document.getElementById("signuppass").value;
-    if (pwTxt.length < 8 || pwTxt.length > 20) {
-      alert("비밀번호 글자 수를 확인해 주세요.")
-      return false;
-    }
-    // 비밀번호 일치 검사
-    let PwChkTxt = document.getElementById("signuppass2").value;
-    if (PwChkTxt != pwTxt) {
-      alert("입력하신 비밀번호를 확인해주세요.")
-      return false;
-    }
-    
-    document.getElementById("joinusername").value = idTxt;
-    document.getElementById("joinpassword1").value = pwTxt;
-    document.getElementById("joinpassword2").value = PwChkTxt;
-    document.getElementById("signupForm").submit();
+  document.getElementById("userName").value = loginname;
+  document.getElementById("passWord").value = loginpassword;
+  document.getElementById("loginForm").submit();
+  
+  }
+
+
+function SignupFormCheck() {
+  // 무입력 아이디 검사
+  let idTxt = document.getElementById("signupName").value;
+  if (idTxt==0 || idTxt=="") {
+    alert("회원가입에 사용하실 ID를 입력해주세요.")
+    return false;
+  // 4이상 12이하의 아이디 검사
+  }
+  if (idTxt.length < 4 || idTxt.length > 12) {
+    alert("ID 글자 수를 확인해 주세요.");
+    return false;
+  }
+  let pwTxt = document.getElementById("signupPass").value;
+  // 무입력 비밀번호 검사
+  if (pwTxt==0 || pwTxt=="") {
+    alert("회원가입에 사용하실 PW를 입력해주세요.")
+    return false;
+  }
+  let PwChkTxt = document.getElementById("signupPass2").value;
+  // 비밀번호 글자수 검사
+  if (pwTxt.length < 8 || pwTxt.length > 20) {
+    alert("PW 글자 수를 확인해 주세요.")
+    return false;
+  }
+  // 비밀번호 일치 검사
+  if (PwChkTxt != pwTxt) {
+    alert("입력하신 비밀번호를 확인해주세요.")
+    return false;
+  }
+  document.getElementById("joinUserName").value = idTxt;
+  document.getElementById("joinPassword1").value = pwTxt;
+  document.getElementById("joinPassword2").value = PwChkTxt;
+  document.getElementById("signupForm").submit();
+  alert("회원가입이 완료되었습니다.")
   }
   
+
+  // function loginFormcheckMent() {
+  //   alert("환영합니다.");
+  //   return true;
+  // }
+
+
+  // function signupFormcheckMent() {
+  //   alert("회원가입이 완료되었습니다.");
+  //   return true;
+  // } 
   // $("#loginForm").click(function(){
   //   alert("환영합니다.")
   // });
-  function loginFormcheckment() {
-    alert("환영합니다.");
-
-    return true;
-  }
-  // $("#signupForm").click(function(){
-  //   alert("회원가입이 완료되었습니다.")
-  //   return true
-  // });
-  
-  function signupFormcheckment() {
-    alert("회원가입이 완료되었습니다.");
-
-    return true;
-  } 
+// $("#signupForm").click(function(){
+//   alert("회원가입이 완료되었습니다.")
+//   return true
+// });
