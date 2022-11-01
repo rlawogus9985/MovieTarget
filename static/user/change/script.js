d58a8a3b9c29ee1b60b823a7e4347fbb5f6271a5
KRK2S <i0kjkk@naver.com>
2022-11-01 16:02:58 +0900

    2022:11:01
    -수정한내용-
    	1. Templates/user/user_change_page.html
    	Input태그의
    	value="username"
    	value="password"
    	이유 : 삭제 paceholder를 보이기위한 수정
    
    	2. User.urls.py & User.views.py
    	# path('<int:auth_user_id>/change_user/', views.UserChangeViews.as_view() ,name='change_user'),
    	와 관련된  주석 되어있는 UserChangeViews 클래스 삭제
    
    	# path('change_user/', views.userchangeview ,name='change_user'),
    	와 관련된  주석 되어있고 쓰지않는 이름만 같은 다른 userchageview  삭제
    	연결되지않는 주석되어있는 이름만 같은 다른 userchageview 삭제
    
    	Url과 연결되지않는 사용하지않는 주석되어있는 delete_user 함수view 삭제
    
    	새로운 로그인관련 예외처리를위한 로그인관련클래스뷰 테스트중
    
    	3. Templates/user/ 사용하지 않는 템플릿 삭제
    	Login.html 삭제이유: Generic.LoginVeiw 가 template/dist/index.html로 연결되기에 쓸모없음.
    	Register.html 사용하지 않는 백지의 템플릿 삭제
    	user_change_page_test.html 테스트용 유저체인지 html 삭제
    	user_form.html  template/dist/index.html에서 로그인과회원가입을 동시에 진행하므로 필요하지 않아서 삭제.
    
     4.  정상작동하는 userchageview
    If 조건문 수정
    username == request.user.username and password ==password2:
    
    If static/user/change/scripts.js를 script.js로 변경함. 오탈자수정
    
    회원정보수정시의 유효성 예외기능 구현
    Menubar.html my project-1.png 하드코딩되어서 회원정보수정후 로고꺠지는부분 static으로 동적으로 가능하도록 수정함.
    
    User_change_page.html comment주석 삭제함
    Form태그에서 button태그 사용하기위해서 form태그에 ousubmit속성을 ='return fasle'로 비활성화설정했음
    
    등등기타더있음...

---
+++ static/user/change/script.js
@@ -0,0 +1,35 @@
+function changeFormIdCheckMent() {
+    // 회원정보 변경시 예외처리기능
+    let changename = document.getElementById("changeName").value;
+    let originname = document.getElementById("originName").value;
+    let changepassword1 = document.getElementById("changePassword1").value;
+    let changepassword2 = document.getElementById("changePassword2").value;
+
+    if (changename==0 || changename=='') {
+        alert("ID를 입력해주세요.");
+        return false;
+    } else if (originname != changename) {
+        alert("접속중인 ID와 수정하려는 ID가 불일치합니다.");
+        return false;
+    } else if (changepassword1==0 || changepassword1=='') {
+        alert("PW를 입력해주세요.");
+        return false;
+    } else if (changepassword1.length < 8 || changepassword1.length > 20) {
+        alert("PW는 8자이상 20자 미만 입니다. PW 글자 수를 확인해 주세요.")
+        return false;
+    } else if (changepassword1 != changepassword2) {
+        alert("비밀번호를 다시 확인해 주세요.");
+        return false;
+    }
+    document.getElementById("changeName").value = changename
+    document.getElementById("originName").value = originname
+    document.getElementById("changePassword1").value = changepassword1
+    document.getElementById("changePassword2").value = changepassword2
+    alert("회원정보수정이 완료되었습니다.")
+    document.getElementById("userChange").submit();
+
+}
+
+// document.getElementsByClassName('w-btn-outline w-btn-gray-outline').addEventListener('click', e => {
+//     window.history.forward();
+//   })
--- user/views.py
+++ user/views.py
@@ -1,6 +1,4 @@
-from asyncio import events
-from multiprocessing import context
-from wsgiref.util import request_uri
+from django.http import Http404, JsonResponse
 from django.shortcuts import  redirect, render, get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views import generic
@@ -12,7 +10,8 @@ from .forms import CheckPasswordForm, CustommUserChangeForm
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
-
+from django.contrib.auth.models import AbstractUser
+from django.views import View
 
 from user.decorators import * # 함수형 뷰 데코
 # from django.utils.decorators import method_decorator # 클래스기반뷰에사용 데코
@@ -43,116 +42,49 @@ def userchangepage(request):
 @login_required(login_url=reverse_lazy('user:login'))
 def  userchangeview(request, auth_user_id):
     # auth_user.username 테이블 필드명 
-
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
-    
+    password2 = request.POST.get('password2', '')
     try:
-        if username == request.user.username:
+        if username == request.user.username and password == password2:
             user = auth_views.UserModel.objects.get(pk=auth_user_id)
             user.set_password(password)
             user.save()
+            return render(request, 'user/profile.html')
         else:
-            print("일치하는 회원이 없습니다.")
-    except:
-        pass
-
-    # AuthviewsUsermodel = auth_views.UserModel.objects.filter(pk=auth_user_id)  
-    # AuthviewsUsermodel.objects.update(password=password)
-    return redirect('user:profile')
-
-#############################################
-# @login_required
-# @require_http_methods(['GET','POST'])
-# def userchangeview(request):
-#     if request.method == 'POST':
-#         form = CustommUserChangeForm(request.POST, instance=request.user)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('user:profile')
-#     else:
-#         form = CustommUserChangeForm(instance=request.user)
-#     context = {
-#         'form':form,
-#     }
-#     return render(request, 'user/user_change_page.html', context)
-
-####################################
-
-# @login_required
-# def userchangeview(request):
-#     if request.method == 'POST':
-#         form = PasswordChangeForm(request.user, request.POST)
-#         if form.is_valid():
-#             form.save()
-#             update_session_auth_hash(request, form.user)
-#             return redirect('user:profile')         
-#     else:
-#         form = PasswordChangeForm(request.user)
-#     context = {
-#         'form' : form,
-#     }
-#     return render(request, 'user/user_change_page.html', context)
-
-
-##############################################
-
-# class UserChangeViews(generic.UpdateView):
-#     models = auth_views.UserModel
-#     context_object_name = 'password'
-# #     form_class = auth_forms.UserChangeForm 
-#     template_name = 'user/user_change_page.html'
-#     success_url = reverse_lazy('movie:board') # post 방식일 경우 리다이렉트로 로그인으로 가는상황
-# #     def get_queryset(self, auth_user_id):
-# #         # changename = self.request.POST.get('changeName')
-
-# #         originalname = self.request.POST.get('username')
-# #         username = auth_views.UserModel.objects.username
-        
-# #         changepassword = self.request.POST.get('password')
-# #         userid = auth_views.UserModel.objects.get(pk=auth_user_id)
-        
-# #         if originalname == username:
-# #             auth_views.UserModel.objects.update('password' = changepassword)
+            return render(request, "user/user_change_page.html")
             
-# #         else:
-# #             pass            
+    except:
+        return render(request, 'user/profile.html')
+    
+## 테스트중인 새로운 클래스뷰
+# @login_required(login_url=reverse_lazy('user:login'))
+# class LoginClassView(View):
+#     model = auth_views.UserModel
+#     fields = ['username', 'password']
+#     template_name = 'dist/index.html'
+    
+    
+    # def post(self, request):
+    #     data = json.loads(request.body)
+    #     try:
+    #         username    = data['username']
+    #         password = data['password']
 
-# #         if changepassword == auth_forms.UserModel.password and originalname == auth_forms.UserModel.username:
-# #             auth_forms.UserModel.password = changepassword
-# #         # return super().get_queryset()
-# #         return 
-#     @ login_required
-#     def get_queryset(self, auth_user_id):
-#         username = self.request.POST.get('username', '')
-#         password = self.request.POST.get('password', '')
-#         pk = self.kwargs.get('auth_user_id')
-#         target = auth_views.UserModel.obejcts.get(pk=pk)
-#         target.password = password
-#         target.save()
-#         return 
-        
-######################################################
+    #         if not auth_views.UserModel.objects.filter(username=username).exists():
+    #             return JsonResponse({'error': 'INVALID_USER'}, status=401)
+    #         if auth_views.UserModel.objects.get(username=username).password == password:
+    #             return JsonResponse({'message':'SUCCESS'}, status=200)
+    #         return JsonResponse({'error': 'INVALID_USER'}, status=401)
 
+    #     except KeyError:
+    #         return JsonResponse({'error': 'KEY_ERROR'}, status=400)
 
 @require_POST
 @login_required(login_url=reverse_lazy('user:login'))
 def delete_user(request):
     request.user.delete()
+    # request.user.is_active()
+
     return redirect('user:join')
-
-# @login_required(login_url=reverse_lazy('user:login'))
-# def delete_user(request):
-#     if request.method == 'POST':
-#         password_form = CheckPasswordForm(request.user, request.POST)
-        
-#         if password_form.is_valid():
-#             request.user.delete()
-#             logout(request)
-#             messages.success(request, "회원탈퇴가 완료되었습니다.")
-#             return redirect('/user/login/')
-#     else:
-#         password_form = CheckPasswordForm(request.user)
-
-#     return render(request, 'user/user_delete.html', {'password_form':password_form})
 
